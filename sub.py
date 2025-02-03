f8fdf15818252ffb035ef1066e8f7ca86dfd04a4
Aditya-Maller <155645339+Aditya-Maller@users.noreply.github.com>
2025-02-03 16:02:07 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,11 +1,1 @@
-# Subtraction Program
-def subtract_numbers(num1, num2):
-    return num1 - num2
 
-# Taking input from the user
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-# Call the function and print the result
-result = subtract_numbers(num1, num2)
-print(f"The difference between {num1} and {num2} is {result}")
